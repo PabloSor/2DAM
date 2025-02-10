3e2647c38df13ba4c21da1408e657e32a9078544
psorianoa <psorianoa@iesch.org>
2025-02-10 21:18:09 +0100

    n

---
+++ Interfaces/2EV/UD04/DistribucionDeApps/src/main/java/org/example/distribuciondeapps/HelloController.java
@@ -0,0 +1,10 @@
+package org.example.distribuciondeapps;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Button button;
+}
